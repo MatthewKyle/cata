b256ab64ca0ce6ab47bb96aa0b6f65c1f0191e62
Matthew J Kyle <MatthewKyle@users.noreply.github.com>
2024-05-02 07:51:03 -0500

    charge procmask
    added procmask to charge

--- sim/warrior/charge.go
+++ sim/warrior/charge.go
@@ -14,6 +14,7 @@ func (warrior *Warrior) RegisterCharge() {
 		SpellSchool:    core.SpellSchoolPhysical,
 		Flags:          core.SpellFlagAPL,
 		ClassSpellMask: SpellMaskCharge,
+		ProcMask:       core.ProcMaskMeleeSpecial,
 
 		Cast: core.CastConfig{
 			DefaultCast: core.Cast{},
